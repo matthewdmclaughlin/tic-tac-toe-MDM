16ad6f58ea3f67409253406032f183e9ada14121
matthewdmclaughlin <matthewdmclaughlin@gmail.com>
2019-06-26 16:15:31 -0400

    Added more files to file structure Whether or not I need them all remains to be seen!

---
+++ assets/scripts/examples/ui.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const successMessage = message => {
+  $('#message').text(message)
+  $('#message').removeClass('failure')
+  $('#message').addClass('success')
+  // Clear out our getFormFields
+  $('form').trigger('reset')
+}
+const failureMessage = message => {
+  $('#message').text(message)
+  $('#message').removeClass('success')
+  $('#message').addClass('failure')
+  // Clear out our getFormFields
+  $('form').trigger('reset')
+}
+
+const createExampleSuccessful = responseData => {
+  // successMessage('You have created an example successfully!')
+  successMessage(`Created: ${responseData.example.text}`)
+}
+const createExampleFailure = responseData => {
+  failureMessage('Create example failed')
+}
+
+module.exports = {
+  createExampleFailure,
+  createExampleSuccessful
+}
